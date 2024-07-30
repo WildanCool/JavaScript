9519bd05b3192107f7313ca7a76a7f44379dd91b
WildanCool <wildan2512@student.abudzar.sch.id>
2024-07-30 11:27:24 +0700

    update
    latihan 2 selesai

--- Meeting 1/coba-nodejs.js
+++ Meeting 1/coba-nodejs.js
@@ -1,1 +1,6 @@
-console.log('Testinggg')
+console.log("Testinggg");
+console.log("");
+
+const tetap = "tidak berubah";
+tetap = "dapat berubah";
+console.log(tetap)
---
+++ Meeting 2/latihan2.js
@@ -0,0 +1,21 @@
+// Nomer 1
+var ada;
+console.log(ada);
+
+// Nomer 2
+var nama = "Wildan";
+var umur = "16";
+var tempattinggal = "Cisauk";
+var p = " ";
+console.log(nama + p + umur + p + tempattinggal);
+
+// Nomer 3
+var a = null;
+console.log(a);
+
+// Nomer 4
+let alas = 5;
+let tinggi = 7;
+let rumus = 1 / 2;
+var satuan = " cm2";
+console.log(rumus * alas * tinggi + satuan);
