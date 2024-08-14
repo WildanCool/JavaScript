9934d42b284ebdbbfb60efc842d9d6bf95e7639c
WildanCool <wildan2512@student.abudzar.sch.id>
2024-08-14 11:18:30 +0700

    update latihan 7

--- Meeting 7/latihan7.js
+++ Meeting 7/latihan7.js
@@ -41,4 +41,35 @@ var myBook = new book("JavaScript Essentials", "Jane Doe");
 console.log(myBook.getDetails());
 
 myBook.updateAuthor("Wildan");
-console.log(myBook.getDetails())
+console.log(myBook.getDetails());
+
+// Essay Nomer 1
+const abudzar = {
+  kepsek: "UstYahya",
+  umur: 22,
+  kota: "Madinah",
+};
+
+console.log(abudzar.kota);
+console.log(abudzar["kota"]);
+
+var usia = abudzar.umur + 5;
+console.log(usia);
+var usia = abudzar["umur"] + 5;
+console.log(usia);
+
+// Essay Nomer 2
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+const myProduct = new Product("Keyboard", 100000);
+const myProduct2 = new Product("Mouse", 50000);
+const myProduct3 = new Product("Headset", 200000);
+
+console.log(myProduct);
+console.log(myProduct2);
+console.log(myProduct3);
