75a8f5e581d204ab306aaf5299e7e092da04a6a7
WildanCool <wildan2512@student.abudzar.sch.id>
2024-10-28 14:12:12 +0700

    update
    LATIHAN 21

---
+++ Meeting 21/script21.js
@@ -0,0 +1,81 @@
+// LOCAL STORAGE
+const localStorageKey = "PRESS_FREQUENCY";
+const nameKey = "NAME";
+
+// Pengecekan: apakah data localStorage dengan key count tersedia atau belum
+if (typeof Storage !== "undefined") {
+  if (localStorage.getItem(localStorageKey) === null) {
+    // Jika item pada local storage belum ada, data akan diberi nilai awal, yakni 0
+    localStorage.setItem(localStorageKey, 0);
+  }
+  const incrementButton = document.querySelector("#incrementButton");
+  const clearButton = document.querySelector("#clear");
+  const countDisplay = document.querySelector("#count");
+
+  // Memberikan nilai item dari local storage
+  countDisplay.innerText = localStorage.getItem(localStorageKey);
+
+  // Update nilai dari item local storage jika tombol ditekan
+  incrementButton.addEventListener("click", function () {
+    let count = localStorage.getItem(localStorageKey);
+    count++;
+    localStorage.setItem(localStorageKey, count);
+    countDisplay.innerText = localStorage.getItem(localStorageKey);
+  });
+
+  // Memberikan nilai 0 ke tampilan karena di-reset dan menghapus item
+  clearButton.addEventListener("click", function () {
+    localStorage.removeItem(localStorageKey);
+    countDisplay.innerText = 0;
+  });
+
+  const saveButton = document.getElementById("saveButton");
+  const hapusButton = document.getElementById("hapusButton");
+  const nameInput = document.getElementById("nameInput");
+  localStorage.getItem(nameKey, 0);
+
+  saveButton.addEventListener("click", function () {
+    localStorage.setItem(nameKey, nameInput.value);
+  });
+
+  hapusButton.addEventListener("click", function () {
+    localStorage.removeItem(nameKey);
+  });
+} else {
+  alert("Browser yang Anda gunakan tidak mendukung Web Storage");
+}
+
+// // SESSION STORAGE
+// const sessionStorageKey = 'PRESS_FREQUENCY';
+
+// // Pengecekan: apakah data sessionStorage dengan key count tersedia atau belum
+// if (typeof Storage !== 'undefined') {
+//   if (sessionStorage.getItem(sessionStorageKey) === null) {
+//     // Jika item pada session storage belum ada, datanya akan diatur dengan nilai awal, yakni 0
+//     sessionStorage.setItem(sessionStorageKey, 0);
+//   }
+
+//   const incrementButton = document.querySelector('#incrementButton');
+//   const clearButton = document.querySelector('#clear');
+//   const countDisplay = document.querySelector('#count');
+
+//   // Memberikan nilai item dari session storage
+//   countDisplay.innerText = sessionStorage.getItem(sessionStorageKey);
+
+//   // Mengupdate nilai dari item session storage jika tombol ditekan
+//   incrementButton.addEventListener('click', function () {
+//     let count = sessionStorage.getItem(sessionStorageKey);
+//     count++;
+//     sessionStorage.setItem(sessionStorageKey, count);
+//     countDisplay.innerText = sessionStorage.getItem(sessionStorageKey);
+//   });
+
+//   // Memberikan nilai 0 ke tampilan karena di-reset dan menghapus item
+//   clearButton.addEventListener('click', function () {
+//     sessionStorage.removeItem(sessionStorageKey);
+//     countDisplay.innerText = 0;
+//   });
+// } else {
+//   alert('Browser yang Anda gunakan tidak mendukung Web Storage');
+// }
+
